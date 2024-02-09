d0e19594ef036d4e873da3b8d87c9825ba21c34b
MaxJLevien <max.levien@gmail.com>
2024-02-09 15:21:39 -0800

    Update aqd.py

--- aqd.py
+++ aqd.py
@@ -1,50 +1,24 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
-"""
-Example sketch to connect to PM2.5 sensor with either I2C or UART.
-"""
-
-# pylint: disable=unused-import
 import time
-
+import csv 
+import serial
 
 reset_pin = None
-# If you have a GPIO, its not a bad idea to connect it to the RESET pin
-# reset_pin = DigitalInOut(board.G0)
-# reset_pin.direction = Direction.OUTPUT
-# reset_pin.value = False
-
-
-# For use with a computer running Windows:
-# import serial
-# uart = serial.Serial("COM30", baudrate=9600, timeout=1)
-
-# For use with microcontroller board:
-# (Connect the sensor TX pin to the board/computer RX pin)
-# uart = busio.UART(board.TX, board.RX, baudrate=9600)
 
 # For use with Raspberry Pi/Linux:
-import serial
+
 uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.25)
 
-# For use with USB-to-serial cable:
-# import serial
-# uart = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=0.25)
-
-# Connect to a PM2.5 sensor over UART
 from adafruit_pm25.uart import PM25_UART
 pm25 = PM25_UART(uart, reset_pin)
 
-# Create library object, use 'slow' 100KHz frequency!
-# i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
-# Connect to a PM2.5 sensor over I2C
-# pm25 = PM25_I2C(i2c, reset_pin)
-
 print("Found PM2.5 sensor, reading data...")
+meta_data = ["Time","PM1.0","PM2.5","PM10"]
+file = open("aq_data.csv", "w", newline='')
+writer = csv.writer(file)
+writer.writerow(meta_data)
+start_time = time.time()
+while (time.time() - start_time) < 20:
 
-while True:
-    time.sleep(1)
 
     try:
         aqdata = pm25.read()
@@ -74,3 +48,9 @@ while True:
     print("Particles > 5.0um / 0.1L air:", aqdata["particles 50um"])
     print("Particles > 10 um / 0.1L air:", aqdata["particles 100um"])
     print("---------------------------------------")
+    data_out = [time.time(), aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"]]
+    writer.writerow(data_out)
+    time.sleep(2)
+    
+
+
