ead9c5fd91e8093245bf612186bb81992f2c2645
apingegno <aingegno@berkeley.edu>
2024-03-15 16:00:45 -0700

    Update detection_script.py
    Fulfilling the extra requirments

--- detection_script.py
+++ detection_script.py
@@ -1,21 +1,45 @@
 import time
 import RPi.GPIO as GPIO
+import sys
+import csv
 
 count = 0
+runningCounts = 0
 print("running")
+
+startTime = time.time()
+runTime = int(sys.argv[1])
+
+filename = sys.argv[2]
+file = open(filename, "w", newline='')
+writer = csv.writer(file)
+
+meta_data = ["Counts","Total Counts", "Count Rate (1/s)","Time (Epoch)"]
+writer.writerow(meta_data)
 
 def my_callback(channel):
     global count
+    global runningCounts
+
     count = count + 1
-    print("Pulse Detected at" + str(time.time()))
+    runningCounts = runningCounts + 1
+
+    print("Pulse Detected at " + str(time.time()))
    
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.IN)
 GPIO.add_event_detect(17, GPIO.FALLING, callback=my_callback)
 
-while True:
+while (time.time()-startTime) <= runTime:
     time.sleep(60)
+
     countRate = count/60
+    runningCountRate = runningCounts/(time.time()-startTime)
+
     print("Current Amount of Pulses Detected: " + str(count))
-    print("Count Rate: " + str(countRate))
+    print("Count Rate: " + str(countRate) + " Counts/s")
+    print("Running Count Rate: " + str(runningCountRate) + " Counts/s")
     count = 0
+
+    data_out = [count, runningCounts, countRate, time.time()]
+    writer.writerow(data_out)
