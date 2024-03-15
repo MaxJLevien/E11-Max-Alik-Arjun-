fe7e258ede8f0310e9bce3ae50220ed77550afc7
apingegno <aingegno@berkeley.edu>
2024-03-15 15:41:26 -0700

    Update detection_script.py

--- detection_script.py
+++ detection_script.py
@@ -8,16 +8,12 @@ def my_callback(channel):
     global count
     count = count + 1
     print("Pulse Detected at" + str(time.time()))
+   
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.IN)
+GPIO.add_event_detect(17, GPIO.FALLING, callback=my_callback)
 
 while True:
     time.sleep(60)
     print("Current Amount of Pulses Detected: " + str(count))
     count = 0
-
-    
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(6, GPIO.IN)
-GPIO.add_event_detect(6, GPIO.FALLING, callback=my_callback)
-
- 
-print("Goodbye!")
