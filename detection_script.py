fedeca1f2aec5079ba396420f6ad7bcb910b928d
apingegno <aingegno@berkeley.edu>
2024-03-15 15:39:47 -0700

    Update detection_script.py

--- detection_script.py
+++ detection_script.py
@@ -2,8 +2,8 @@ import time
 import RPi.GPIO as GPIO
 
 count = 0
+print("running")
 
-startTime = time.time()
 def my_callback(channel):
     global count
     count = count + 1
@@ -14,13 +14,10 @@ while True:
     print("Current Amount of Pulses Detected: " + str(count))
     count = 0
 
-try:
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(6, GPIO.IN)
-    GPIO.add_event_detect(6, GPIO.FALLING, callback=my_callback)
- 
- 
-finally:
-    GPIO.cleanup()
+    
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(6, GPIO.IN)
+GPIO.add_event_detect(6, GPIO.FALLING, callback=my_callback)
+
  
 print("Goodbye!")
